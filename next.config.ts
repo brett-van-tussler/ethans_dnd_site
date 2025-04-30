abe387cbf2e1877c90338554a3e49ec1df0696be
Brett  Van Tassel <79537144+brett-van-tussler@users.noreply.github.com>
2025-04-30 13:44:09 -0700

    export static site

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import { NextConfig } from 'next';
 
 const config: NextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true
+  },
+  images: {
+    unoptimized: true
   }
 };
 
